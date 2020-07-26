ef2663d2c11c207275b5d2b79620724a066e223c
Erick Villeda <eeverickeev@gmail.com>
2020-07-26 05:11:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+var recipes = {Object};
+
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, { [key]: value });
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  object.key = 'value';
-  return object;
+  
 }
 function deleteFromObjectByKey(object, key){
   
